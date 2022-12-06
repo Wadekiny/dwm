b739914951ed7ae5a480646d8724bac6d56ec515
Wadekiny <wadekiny@qq.com>
2022-12-06 14:42:26 +0800

    增加状态栏的cal增加正则匹配，去掉_^H

--- autostart.sh
+++ autostart.sh
@@ -3,6 +3,7 @@
 /home/wadekiny/ProgramFiles/picom-wadekiny/build/src/picom --experimental-backends &
 feh --bg-fill ~/Pictures/background/background.png  &
 bash /home/wadekiny/ProgramFiles/dwm-wadekiny/statusbar/statusbar.sh cron &
+#bash /home/wadekiny/ProgramFiles/dwm_yaocccc/statusbar/statusbar.sh cron &
 bash /home/wadekiny/scripts/tap2click.sh &
 bash /home/wadekiny/scripts/inverse-scroll.sh &
 python3 /home/wadekiny/scripts/wallpic.py &
--- statusbar/statusbar.sh
+++ statusbar/statusbar.sh
@@ -2,7 +2,6 @@
 
  
 DWM="/home/wadekiny/ProgramFiles/dwm-wadekiny"
-echo $DWM
 
 touch $DWM/statusbar/temp
 
@@ -25,23 +24,27 @@ click() {
 refresh() {
     _icons=''; _coin=''; _df=''; _cpu=''; _mem=''; _date=''; _vol=''; _bat='';   # 重置所有模块的状态为空
     source $DWM/statusbar/temp                                           # 从 temp 文件中读取模块的状态
+    echo $DWM
+    name="$_icons$_coin$_df$_cpu$_mem$_date$_vol$_bat"
+    echo $name
     xsetroot -name "$_icons$_coin$_df$_cpu$_mem$_date$_vol$_bat"             # 更新状态栏
 }
 
 # 启动定时更新状态栏 不用的package有不同的刷新周期 注意不要重复启动该func
 cron() {
     while true; do update cpu mem ;        sleep 1;  done &              # 每隔20s更新cpu 内存
-    #while true; do update df ;             sleep 1;  done &              # 每隔20s更新cpu 内存
+    while true; do update df ;             sleep 1;  done &              # 每隔20s更新cpu 内存
     #while true; do update coin;           sleep 20;  done &              # 每隔20s更新币价
-    #while true; do update vol icons;      sleep 1;  done &              # 每隔60s更新音量 图标
-    #while true; do update bat;            sleep 30; done &              # 每隔300s更新电池
-    #while true; do update date; refresh;  sleep 5;   done &              # 每隔5s更新时间并更新状态栏
+    while true; do update vol icons;      sleep 1;  done &              # 每隔60s更新音量 图标
+    while true; do update bat;            sleep 30; done &              # 每隔300s更新电池
+    while true; do update date; refresh;  sleep 5;   done &              # 每隔5s更新时间并更新状态栏
 }
 
 case $1 in
     cron) cron; ;;
     refresh) refresh ;;
     update) shift 1; update $*; refresh ;;
+    #updateall) update icons coin df cpu mem date vol bat; refresh ;;
     updateall) update icons coin df cpu mem date vol bat; refresh ;;
     # 接收clickstatusbar传递过来的信号 $1: 模块名  $2: 按键(L|M|R)
     *) click $1 $2 ;;
